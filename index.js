9630c2ca6bc9476f48bbe631279d1d1d915a0b3b
jahir <rana78085@gmail.com>
2022-11-24 22:52:10 +0600

    Add Hp Collection

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -7,23 +9,46 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// db_name = sells
-// db_pass = euzu56pcJHRDuq94
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a8e9r20.mongodb.net/?retryWrites=true&w=majority`;
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.a8e9r20.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+// prettier-ignore
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1,
 });
 
+const usedProductsBrand = client.db("usedProductsSell").collection("brands");
+const productDetails = client
+  .db("usedProductsSell")
+  .collection("productsDetails");
 
-app.get('/', (req, res) => {
-    res.send('Resale Web Server is Running...');
-})
+app.get("/brands", async (req, res) => {
+  try {
+    const brandsProducts = await usedProductsBrand.find({}).toArray();
+    res.send(brandsProducts);
+  } catch {}
+});
+
+app.get("/brands/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { brandId: ObjectId(id) };
+    const result = await productDetails.find(query).toArray();
+    res.send(result);
+  } catch {}
+});
+
+app.get("/productsDetails/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await productDetails.findOne(query);
+    res.send(result);
+  } catch {}
+});
+
+app.get("/", (req, res) => {
+  res.send("Resale Web Server is Running...");
+});
 
 app.listen(port, () => {
-    console.log(`Resale Web Server Running on: ${port}`)
-})
+  console.log(`Resale Web Server Running on: ${port}`);
+});
